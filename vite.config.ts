efb6ee0c89a25b9a12099618f404863013ea1ec9
Chris Battle <cbattle@radar.internal>
2023-09-20 12:33:11 -0500

    fixing module error 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   build: {
     rollupOptions: {
+      external: ['/dist/index.1a386d4b.mjs'],
       output: {
         entryFileNames: `[name].[hash].mjs`,
         chunkFileNames: `[name].[hash].mjs`,
